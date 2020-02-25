17796568e2c88be3ee768f5422bbfa6bbb748172
Harold Brinkhof <harold.brinkhof@gmail.com>
2020-02-25 22:11:55 +0100

    reworked the querying and database connection part
    noticed that via wsgi, database connections seem to get dropped a lot and not restarted, leading
    to quite weird unrelated SSL errors between the wsgi app and postgresql. Since this is not a high traffic
    app, remade so the connection is started and closed for every call made. Slow but certain. :-/
    
    revamped the query code because the retardation that is handling having no data to return from a legitimate
    as an error forcing me to catch it.. better have different methods then, with a cludgy hack to determine if we
    have any rows to return from the SELECT issueing one.
    
    God knows why I don't like python for anything that's not a shellscript..

--- pairing.py
+++ pairing.py
@@ -11,18 +11,39 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 dotenv.load_dotenv(dotenv_path)
 verification_token = os.environ['VERIFICATION_TOKEN']
 
-conn = psycopg2.connect(host='localhost',database=os.environ['DBNAME'],user=os.environ['USERNAME'],password=os.environ['PASSWORD'])
-cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor);
 
 def performQuery(sqlText, params):
+    conn = psycopg2.connect(host='localhost',database=os.environ['DBNAME'],user=os.environ['USERNAME'],password=os.environ['PASSWORD'])
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor);
+    rows = None
+
     try:
         cur.execute(sqlText,params)
+        if cur.statusmessage != 'SELECT 0':
+            rows = cur.fetchall()
+        conn.commit()
     except Exception as e:
         print(e)
         conn.rollback()
 
-    else:
+    cur.close()
+    conn.close()
+    return rows
+
+def performUpdate(sqlText, params):
+    conn = psycopg2.connect(host='localhost',database=os.environ['DBNAME'],user=os.environ['USERNAME'],password=os.environ['PASSWORD'])
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor);
+
+    try:
+        cur.execute(sqlText,params)
         conn.commit()
+    except Exception as e:
+        print(e)
+        conn.rollback()
+
+    cur.close()
+    conn.close()
+
 
 @app.route("/")
 def hello():
@@ -44,23 +65,23 @@ def pairing():
 
         if len(text) == 0:
             # we return a list of the users available for pairing
-            performQuery("""SELECT * FROM availability 
+            rows = performQuery("""SELECT * FROM availability 
                                             WHERE still_available = TRUE AND offered_on = CURRENT_DATE 
                                             AND channel_id = %s""",(channel_id,))
             persons = ''
-            rows = cur.fetchall()
-            for row in rows:
-                persons += '. ' + row['person_name'] + "\n"
             
-            if len(rows) == 0:
+            if rows == None or len(rows) == 0:
                 persons = '_nobody is available for pairing_ :disappointed:'
+            else:
+                for row in rows:
+                    persons += '. ' + row['person_name'] + "\n"
 
             payload = {'type':'mrkdwn','response_type':'ephemeral','text':'Available for pairing in ' + channel_name + "\n" + persons}
 
         elif text == 'yes' or text == 'on':
             # we mark this person as being available for pairing in this channel
             # set still_available on true again where a conflict has occurrec: UPSERT
-            performQuery("""INSERT INTO availability (channel_id,channel_name,person_id,person_name) 
+            performUpdate("""INSERT INTO availability (channel_id,channel_name,person_id,person_name) 
                                     VALUES (%s,%s,%s,%s) 
                                     ON CONFLICT (channel_id, person_id, offered_on) DO UPDATE SET still_available = TRUE""",
                                     (channel_id,channel_name,
@@ -69,12 +90,12 @@ def pairing():
         elif text == 'no' or text == 'off':
             # we remove this person of the list of available people to pair with for this channel
             # we don't care to;limit it to the current day since we don't care about previous data here
-            performQuery("""UPDATE availability SET still_available = FALSE 
+            performUpdate("""UPDATE availability SET still_available = FALSE 
                                     WHERE  channel_id = %s AND person_id = %s""",
                                     (channel_id,user_id))
             payload = {'response_type':'ephemeral', 'text':'You have been unregistered as available for pairing in ' + channel_name}
         else:
-            payload = {'response_type': 'ephemeral','text':'unknown option: ' + text + display_name}
+            payload = {'response_type': 'ephemeral','text':'unknown option: ' + text + user_name}
 
     else:
         payload = {'response_type':'ephemeral','text':'token authorization failed'};
@@ -102,7 +123,7 @@ def pairingWith():
     if 'text' in request.form and len(request.form['text']) > 0:
         pattern = re.compile('^<@[^ ]+>$') # apparantly still follows <@userid|username>  slack api is truly horrible
         if pattern.match(request.form['text']) != None: # make sure that what we got passed was a username
-            performQuery("INSERT INTO pairings (person_id, pairs_with) VALUES(%s,%s)",(user_id,text));
+            performUpdate("INSERT INTO pairings (person_id, pairs_with) VALUES(%s,%s)",(user_id,text));
             payload = {'response_type':'ephemeral','type':'mrkdwn','text':'you are registered as pairing with' + request.form['text']}
         else:
             payload = {'response_type':'ephemeral','type':'mrkdwn','text':'Unknown command option: ' + request.form['text']}
@@ -120,7 +141,7 @@ def pairingEnd():
     user_name = "" if 'user_name' not in request.form else request.form['user_name']
     user_id = "" if 'user_id' not in request.form else request.form['user_id']
 
-    performQuery("UPDATE pairings SET ending = now() WHERE person_id = %s AND starting = ending;",(user_id,))
+    performUpdate("UPDATE pairings SET ending = now() WHERE person_id = %s AND starting = ending;",(user_id,))
     payload = {'response_type':'ephemeral','type':'mrkdwn','text':'You finished pairing'}
     return jsonify(payload)
 
@@ -128,7 +149,4 @@ def pairingEnd():
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', debug=True)
-    if conn is not None:
-        cur.close()
-        conn.close()
 
